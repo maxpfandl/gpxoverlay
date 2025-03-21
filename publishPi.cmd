6bc006582a359929e72f79c33bf68b63883d5305
Max Pfandl <max@pfandl.name>
2025-03-21 14:57:34 +0100

    update

--- publishPi.cmd
+++ publishPi.cmd
@@ -3,5 +3,5 @@ REM dotnet publish -c Release -o \\192.168.0.15\docker\rssfeed\bin\
 rmdir /q /s d:\Documents\Development\publishfolder\
 dotnet publish -c Release -r ubuntu-arm64 -o d:\Documents\Development\publishfolder\ --self-contained false
 cd d:\Documents\Development\publishfolder\
-wsl rsync --no-perms --no-owner --no-group -a -v /mnt/d/Documents/Development/publishfolder/ piServer:/home/madmap/deployscripts/gpx/
-ssh piServer '/home/madmap/deployscripts/gpxoverlay.sh'
+wsl rsync -a -v /mnt/d/Documents/Development/publishfolder/ piServer:/var/dotnet/gpxoverlay/
+
